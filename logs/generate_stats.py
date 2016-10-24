616f2daee52e7d9cf908b43082db568640e69fa8
Samarth <samarth4149@gmail.com>
2016-10-24 20:36:22 +0530

    Changed to print max number of turns

--- logs/generate_stats.py
+++ logs/generate_stats.py
@@ -7,6 +7,7 @@ try:
     file.close()
     raw.pop()
     turns = 0
+    worst_case = 0
     time = 0
     flag = 1
     experiments = len(raw)
@@ -15,11 +16,13 @@ try:
             flag = 0
             experiments -= 1
         else:
-            turns += int(sample.split("Cleared Board in ")
-                         [1].split(" turns")[0])
-            time += float(sample.split("Realtime taken: ")[1].split(" s")[0])
+			curr_turns = int(sample.split("Cleared Board in ")[1].split(" turns")[0])
+			worst_case = max(worst_case, curr_turns)
+			# turns += curr_turns
+			time += float(sample.split("Realtime taken: ")[1].split(" s")[0])
 
-    print "Average turn to clear the board: ", turns/experiments
+    print "Average turn to clear the board: ", float(turns)/experiments
+    print "Maximum number of turns: ", worst_case
     print "Total time taken: ", time
     print "Number of Experiments: ", experiments
     if flag == 0:
